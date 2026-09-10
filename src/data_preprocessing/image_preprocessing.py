3f9ee110ab7fe750b91abc6b45bca5f34e74db1d
Cody Gould <crg5809@psu.edu>
2026-09-09 20:51:20 -0400

    Adds data preprocessing for images

--- src/data_preprocessing/image_preprocessing.py
+++ src/data_preprocessing/image_preprocessing.py
@@ -102,7 +102,6 @@ def resize_image(image: np.ndarray, new_size: tuple[int,int]= (500,500)) -> np.n
 def preprocess_image(image_path: str, processed_size: tuple=(500,500)):
     """
     Preprocess a single image
-    Just a placeholder for now, feel free to edit/remove
     """
     image = load_image(image_path)
     if image is None:
